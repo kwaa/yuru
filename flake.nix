b9aae8405c7c34a136370c9a8966252da05e2238
藍+85CD <50108258+kwaa@users.noreply.github.com>
2026-07-16 15:23:31 +0800

    chore: add lake

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "github:kwaa/yuru";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            nativeBuildInputs = [];
+
+            buildInputs = [];
+          };
+        }
+      );
+    };
+}
